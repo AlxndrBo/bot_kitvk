d2ae7c3897491d5cd27d7de8557de7885a7034f8
AlxndrBo <sasha547@ya.ru>
2016-08-23 13:04:00 +0300

    Update bot_kitvk.py

--- bot_kitvk.py
+++ bot_kitvk.py
@@ -1,6 +1,32 @@
 import vk, http.client, re
 from lxml import etree
 
+def URI_Parser(AnyText):
+	result = re.findall(r'steamcommunity.com/(\w+/\w+)', stroka2)
+	if result:
+		result = result[0] + "/?xml=1"
+		print(result)
+		return result
+	else:
+		return -1
+
+def SteamConvert(SteamID): #SteamID id/pinkashker/?xml=1 or profiles/7656119808669356/?xml=1
+	conn = http.client.HTTPConnection("steamcommunity.com")
+	conn.request("GET", "/id/pinkashker/?xml=1")
+	r1 = conn.getresponse()
+	if r1=="200":
+		data1 = r1.read()
+		#tree = etree.parse('filename')
+		tree = etree.fromstring(data1)
+		SteamID64 = tree.xpath("/profile/steamID64/text()")[0]
+		CustomURL = tree.xpath("/profile/customURL/text()")[0]
+		print('SteamID64='+SteamID64)
+		print('CustomURL='+CustomURL)
+		return SteamID64
+	else:
+		return -1
+
+#=====================================================================
 session = vk.Session()
 api = vk.API(session)
 
@@ -19,30 +45,3 @@ for element in response['items']:
 	print(SteamURI)
 	
 #========================================
-
-def URI_Parser(AnyText):
-	result = re.findall(r'steamcommunity.com/(\w+/\w+)', stroka2)
-	if result:
-		result = result[0] + "/?xml=1"
-		print(result)
-		return result
-	else:
-		return -1
-		
-
-def SteamConvert(SteamID): #SteamID id/pinkashker/?xml=1 or profiles/7656119808669356/?xml=1
-	conn = http.client.HTTPConnection("steamcommunity.com")
-	conn.request("GET", "/id/pinkashker/?xml=1")
-	r1 = conn.getresponse()
-	if r1=="200":
-		data1 = r1.read()
-		#tree = etree.parse('filename')
-		tree = etree.fromstring(data1)
-		SteamID64 = tree.xpath("/profile/steamID64/text()")[0]
-		CustomURL = tree.xpath("/profile/customURL/text()")[0]
-		print('SteamID64='+SteamID64)
-		print('CustomURL='+CustomURL)
-		return SteamID64
-	else:
-		return -1
-
