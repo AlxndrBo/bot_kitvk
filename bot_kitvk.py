48448b81bf492d2380f7abe7158d7c48448ab213
AlxndrBo <sasha547@ya.ru>
2016-08-25 16:00:07 +0300

    Update bot_kitvk.py

--- bot_kitvk.py
+++ bot_kitvk.py
@@ -5,10 +5,13 @@ VK_groupID = 112445142
 VK_topicID = 33646520
 
 def URI_Parser(AnyText): # Принимает текст, возвращает ссылку вида id/customURL/?xml=1 or profiles/SteamID64/?xml=1
-	result = re.findall(r'steamcommunity.com/(id|profiles+/\w+)', AnyText)
+	#result = re.findall(r'steamcommunity.com/(w+/\w+)', AnyText)
+	#result = re.findall(r'steamcommunity.com/(id|profiles+/\w+)', AnyText)
+	result = re.findall(r'steamcommunity.com/(id|profiles+/\w+)', AnyText)	
 	if result:
 		result = result[0] + "/?xml=1"
 		print(result)
+		sys.exit()
 		return result
 	else:
 		return -1
