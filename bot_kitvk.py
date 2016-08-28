a97eb2d53ccc46d1adbaf4dae807d163d80a900a
Alexandr <sasha547@ya.ru>
2016-08-28 17:02:55 +0300

    Update bot_kitvk.py

--- bot_kitvk.py
+++ bot_kitvk.py
@@ -2,7 +2,7 @@ import vk, http.client, re, time, socket, sys
 from lxml import etree
 
 #RCON_pwd = input("RCON password: ") # В своем рабочем скрипте можете указать пароль тут. Не кладите пароль на github :)))
-if len(sys.argv):
+if len(sys.argv)!=0:
 	RCON_pwd = sys.argv[1] # Get RCON_pwd from command line param. For start: bot_kitvk.py rconpassword
 	print(RCON_pwd)
 else:
